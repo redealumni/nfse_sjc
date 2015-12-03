077bed81e5abce226c9d87d0f2002dee19fdde7c
Daniel Gracia <daniel.santos@redealumni.com>
2015-12-03 16:33:02 -0200

    Fix header issues

--- lib/nfse.rb
+++ lib/nfse.rb
@@ -64,7 +64,7 @@ module NFSe
     def call(method, params)
       @savon.call(method, message: {
         arg0: @header,
-        arg1: Document.new(Dirs.template("#{method}.xml.erb"), params)
+        arg1: Document.new(Dirs.template("#{method}.xml.erb"), params).to_xml
       })
     end
 
