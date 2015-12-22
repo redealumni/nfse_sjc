f16f45141ad0e06629ca38fe1da29f4c68012e35
Thiago Brandão <thiagobrandam@gmail.com>
2015-12-22 19:04:45 -0200

    Hashifies inner arrays as well

--- lib/nfse_sjc/response_hash.rb
+++ lib/nfse_sjc/response_hash.rb
@@ -6,7 +6,18 @@ module NfseSjc
 
     def to_deep_symbolized_h
       @hash.inject(Hash.new) do |h, (k, v)|
-        h.merge!({k.snakecase.to_sym => (v.is_a?(Hash) ? ResponseHash.new(v).to_deep_symbolized_h : v)})
+        h.merge!({
+          k.snakecase.to_sym => (
+            case v
+            when Hash
+              ResponseHash.new(v).to_deep_symbolized_h
+            when Array
+              v.map{|item| ResponseHash.new(item).to_deep_symbolized_h}
+            else
+              v
+            end
+          )
+        })
       end
     end
   end
